f3a232f52a73add1e6937ed0ff4c55f68a836dc9
lim ho <dlagh123@gmail.com>
2023-04-09 20:13:23 +0900

    로딩페이지 추가

--- src/App.vue
+++ src/App.vue
@@ -1,124 +1,29 @@
 <template>
-  <div class="window">
-    <IconList :ICON_LIST="ICON_LIST" />
-    <Footer />
-    <button class="fullSizeBtn" @click="onClickFullsize">
-      <img :src="fullscreen" alt="fullscreenIcon" />
-    </button>
+  <div>
+    <WidonwLoading v-show="isWindowLoading" />
+    <Window v-show="!isWindowLoading" />
   </div>
 </template>
 <script lang="ts">
-import { defineAsyncComponent, defineComponent, Ref, ref } from "vue";
-import fullscreen from "./assets/images/fullscreen.png";
-import { iconType } from "./types/iconType";
+import { defineAsyncComponent, defineComponent, ref } from "vue";
 export default defineComponent({
   components: {
-    IconList: defineAsyncComponent(
-      () => import("./components/windowIcons/IconList.vue")
-    ),
-    Footer: defineAsyncComponent(
-      () => import("./components/footer/Footer.vue")
+    Window: defineAsyncComponent(() => import("./views/Window.vue")),
+    WidonwLoading: defineAsyncComponent(
+      () => import("./views/WindowLoading.vue")
     ),
   },
   setup() {
-    const onClickFullsize = () => {
-      if (!document.fullscreenElement) {
-        document.documentElement.requestFullscreen();
-      } else {
-        if (document.exitFullscreen) document.exitFullscreen();
-      }
+    const isWindowLoading = ref(true);
+    const onCreated = () => {
+      setTimeout(() => {
+        isWindowLoading.value = false;
+      }, 3000);
     };
-    const moveGithub = () =>
-      window.open("https://github.com/limhoooo", "_blank");
-    const moveYoutube = () =>
-      window.open("https://limhoooo-react-youtube.netlify.app/", "_blank");
-    const movetistory = () =>
-      window.open("https://limhoooo.tistory.com/", "_blank");
-
-    const ICON_LIST = ref<iconType[]>([
-      {
-        id: new Date().getTime() + Math.random(),
-        src: new URL(`@/assets/images/computer.png`, import.meta.url).href,
-        alt: "computer icon",
-        name: "내컴퓨터",
-        type: "folder",
-        icons: [
-          {
-            id: new Date().getTime() + Math.random(),
-            src: new URL(`@/assets/images/tistory_icon.png`, import.meta.url)
-              .href,
-            alt: "tistory icon",
-            name: "My Tistory",
-            type: "link",
-            onMoveLink: movetistory,
-          },
-          {
-            id: new Date().getTime() + Math.random(),
-            src: new URL(`@/assets/images/github_logo.png`, import.meta.url)
-              .href,
-            alt: "gitHub icon",
-            name: "My GitHub",
-            type: "link",
-            onMoveLink: moveGithub,
-          },
-        ],
-      },
-      {
-        id: new Date().getTime() + Math.random(),
-        src: new URL(`@/assets/images/folder.png`, import.meta.url).href,
-        alt: "folder icon",
-        name: "내 폴더",
-        type: "folder",
-      },
-      {
-        id: new Date().getTime() + Math.random(),
-        src: new URL(`@/assets/images/github_logo.png`, import.meta.url).href,
-        alt: "gitHub icon",
-        name: "GitHub",
-        type: "link",
-        onMoveLink: moveGithub,
-      },
-      {
-        id: new Date().getTime() + Math.random(),
-        src: new URL(`@/assets/images/notepad.png`, import.meta.url).href,
-        alt: "notepad icon",
-        name: "메모장",
-        type: "notepad",
-      },
-      {
-        id: new Date().getTime() + Math.random(),
-        src: new URL(`@/assets/images/youtube-icon.png`, import.meta.url).href,
-        alt: "youtube icon",
-        name: "YouTube",
-        type: "link",
-        onMoveLink: moveYoutube,
-      },
-    ]);
-
+    onCreated();
     return {
-      ICON_LIST,
-      fullscreen,
-      onClickFullsize,
+      isWindowLoading,
     };
   },
 });
 </script>
-
-<style scoped>
-.window {
-  width: 100%;
-  height: 100%;
-}
-.fullSizeBtn {
-  position: absolute;
-  right: 40px;
-  top: 30px;
-  background: none;
-  border: none;
-}
-.fullSizeBtn img {
-  width: 26px;
-  filter: opacity(0.6);
-  cursor: pointer;
-}
-</style>
---
+++ src/views/WindowLoading.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="loading__container">
+    <div class="loading__container__section">
+      <div>
+        <img :src="logo" alt="widowXp_loading_icon" />
+        <div class="loading">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import logo from "../assets/images/widowXp_loading_icon.png";
+export default {
+  setup() {
+    return {
+      logo,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.loading__container {
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  background-color: #012f9e;
+  z-index: 9999;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.loading__container .loading__container__section {
+  width: 100%;
+  height: 80%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #567dda;
+  border-top: 1px solid #a1c1ff;
+  border-bottom: 1px solid #a1c1ff;
+}
+.loading {
+  text-align: center;
+  margin-top: 50px;
+}
+.loading span {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background-color: gray;
+  border-radius: 50%; /* span을 동그랗게 */
+  animation: loading 1s 0s linear infinite;
+}
+.loading span {
+  background-color: #eee;
+  margin-right: 8px;
+}
+.loading span:nth-child(1) {
+  animation-delay: 0s;
+}
+.loading span:nth-child(2) {
+  animation-delay: 0.2s;
+}
+.loading span:nth-child(3) {
+  animation-delay: 0.4s;
+}
+.loading span:nth-child(4) {
+  animation-delay: 0.6s;
+}
+.loading span:nth-child(5) {
+  animation-delay: 0.8s;
+}
+@keyframes loading {
+  0%,
+  100% {
+    opacity: 0.6;
+    transform: scale(0.5);
+  }
+  50% {
+    opacity: 1;
+    transform: scale(1.2);
+  }
+}
+</style>
