826e750d44382d56e34bd8f8310cb8e6d9a78699
lim ho <dlagh123@gmail.com>
2023-04-10 09:29:20 +0900

    .

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,6 @@ import "./style.css";
 import App from "./App.vue";
 import { createPinia } from "pinia";
 const pinia = createPinia();
-
 const app = createApp(App);
-
 app.use(pinia);
-
 app.mount("#app");
